feeace713eb10f4891bace62e0a7a9a3ab5d2115
Mark Freedman <mark.freedman@gmail.com>
2017-02-18 21:25:28 -0500

    remove json2html as I wanted to use some css here to fancy it up a bit. add w3schools css

---
+++ src/main/webapp/js/student-records.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "/students/list"
+    }).then(function(data) {
+        console.log(data);
+
+        var ul = $('<ul class="w3-ul w3-card-4 w3-green">').appendTo('.student-content');
+        var json = data;
+        console.log(json);
+        // for each json item in the array, create a <li> element with some data
+        $(json).each(function(index, item) {
+            console.log(item);
+            ul.append(
+                $(document.createElement('li')).text(item["name"] + " was born " + item["DOB"]));
+        });
+    });
+});
