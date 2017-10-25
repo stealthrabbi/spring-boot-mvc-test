d6aa43a0afc6867252334c756f9d26336c99dda9
Mark Freedman <mark.freedman@gmail.com>
2017-10-25 17:57:01 -0400

    initial attempt at ag-grid

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular/main';
+import { GridOptions, RowNode } from 'ag-grid/main';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,40 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'app a4';
   studentList = {};
+  gridOptions = {};
+
 
    constructor(private http: HttpClient){
+       this.gridOptions = <GridOptions>{
+           context: {
+               componentParent: this
+           },
+           columnDefs: this.createColumnDefs(),
+           rowDefs: this.studentList,
+       };
    }
 
    ngOnInit(): void {
      this.http.get('students/list').subscribe(data => {
        this.studentList = data;
        console.log(data);
+       this.gridOptions.api.setRowData(this.studentList);
      });
    }
+
+       private createColumnDefs() {
+           return [
+               {
+                   headerName: "Name",
+                   field: "name",
+                   width: 400
+               },
+               {
+                   headerName: "SSN",
+                   field: "ssn",
+                   width: 400,
+               }
+           ];
+       }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// ag-grid
+import {AgGridModule} from "ag-grid-angular/main";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
